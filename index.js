5b5bd1aca9f8877654d957fc1f89dc71d8bc49bc
Noah <noahhughesph@gmail.com>
2023-06-06 22:10:01 -0400

    all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+const saturdayFun = (activity) => {
+  if (!activity) {
+    return 'This Saturday, I want to roller-skate!';
+  }
+  return `This Saturday, I want to ${activity}!`;
+};
+
+const mondayWork = (activity) => {
+  if (!activity) {
+    return 'This Monday, I will go to the office.';
+  }
+  return `This Monday, I will ${activity}.`;
+};
+
+const wrapAdjective = (textFlair) => {
+  return function (adjective) {
+    if (!adjective) {
+      return `You are ${textFlair}special${textFlair}!`;
+    }
+    return `You are ${textFlair}${adjective}${textFlair}!`;
+  };
+};
